0353f16b6f7e34fd2d7386406a9cb96a8dbb775a
Arkadyzja <166837400+Arkadyzja@users.noreply.github.com>
2025-01-23 23:12:04 +0100

    rich player info added to chat

--- inject.js
+++ inject.js
@@ -5,6 +5,7 @@
 
 // CONFIGURATION //
 const CONFIG = {
+    addMorePlayerInfoToChat: true,
     showThemeButtons: true,
     showFavoritesButton: true,
     showFavoriteNavigationButtons: true,
@@ -109,6 +110,157 @@ const addNavigationButtons = (targetElement, prevCallback, nextCallback) => {
     targetElement.parentNode.insertBefore(container, targetElement.nextSibling);
 };
 
+function initializeAddons(imgSrc, url, title) {
+
+    document.querySelectorAll('.logo')[0].innerHTML = '';
+
+
+    const imgWrapper = document.createElement('div');
+    imgWrapper.className = 'imgWrapper';
+    imgWrapper.style.width = '80px';
+    imgWrapper.style.display = 'block'; // Inline box
+    imgWrapper.style.height = '80px';
+    imgWrapper.title = title;
+    imgWrapper.style.backgroundImage = `url('${imgSrc}')`;
+    imgWrapper.style.backgroundSize = 'contain'; // Crop image to center
+    imgWrapper.style.marginLeft = '5px'; // Add some space from the previous element
+    imgWrapper.style.cursor = 'pointer';
+    imgWrapper.onclick = () => {
+        window.open(url, '_blank');
+    }
+    document.querySelectorAll('.logo')[0].appendChild(imgWrapper);
+    return imgWrapper;
+}
+
+function createFlagElement(country) {
+    const flagWrapper = document.createElement('span');
+    flagWrapper.className = 'flagWrapper';
+    flagWrapper.style.width = '20px';
+    flagWrapper.style.display = 'inline-block'; // Inline box
+    flagWrapper.style.height = '14px';
+    flagWrapper.title = country.full_name;
+    flagWrapper.style.backgroundImage = `url('static/flags/${country.iso_code.toLowerCase()}.png')`;
+    flagWrapper.style.backgroundSize = 'contain'; // Crop image to center
+    flagWrapper.style.marginLeft = '5px'; // Add some space from the author name
+    return flagWrapper;
+}
+
+function createPingElement(pingSrc, title) {
+    const pingWrapper = document.createElement('span');
+    pingWrapper.className = 'pingWrapper';
+    pingWrapper.style.width = '15px';
+    pingWrapper.style.display = 'inline-block'; // Inline box
+    pingWrapper.style.height = '15px';
+    pingWrapper.title = title;
+    pingWrapper.style.backgroundImage = `url('${pingSrc}')`;
+    pingWrapper.style.backgroundSize = 'contain'; // Crop image to center
+    pingWrapper.style.marginLeft = '5px'; // Add some space from the previous element
+    return pingWrapper;
+}
+
+function createRankElement(rankSrc, title) {
+    const pingWrapper = document.createElement('span');
+    pingWrapper.className = 'pingWrapper';
+    pingWrapper.style.width = '15px';
+    pingWrapper.style.display = 'inline-block'; // Inline box
+    pingWrapper.style.height = '15px';
+    pingWrapper.title = title;
+    pingWrapper.style.backgroundImage = `url('${rankSrc}')`;
+    pingWrapper.style.backgroundSize = 'contain'; // Crop image to center
+    pingWrapper.style.marginLeft = '5px'; // Add some space from the previous element
+    return pingWrapper;
+}
+
+function createStatusElement(isAway){
+    const statusWrapper = document.createElement('div');
+    statusWrapper.className = 'statusWrapper';
+    statusWrapper.title = isAway ? 'Away' : 'Online';
+    statusWrapper.style.width = '10px';
+    statusWrapper.style.display = 'inline-block'; // Inline box
+    statusWrapper.style.height = '10px';
+    statusWrapper.style.borderRadius = '50%';
+    statusWrapper.style.backgroundColor = isAway ? 'orange' : 'green';
+    statusWrapper.style.marginLeft = '5px'; // Add some space from the previous element
+    return statusWrapper;
+}
+
+function initializeAddons(imgSrc, url, title) {
+
+    document.querySelectorAll('.logo')[0].innerHTML = '';
+
+
+    const imgWrapper = document.createElement('div');
+    imgWrapper.className = 'imgWrapper';
+    imgWrapper.style.width = '80px';
+    imgWrapper.style.display = 'block'; // Inline box
+    imgWrapper.style.height = '80px';
+    imgWrapper.title = title;
+    imgWrapper.style.backgroundImage = `url('${imgSrc}')`;
+    imgWrapper.style.backgroundSize = 'contain'; // Crop image to center
+    imgWrapper.style.marginLeft = '5px'; // Add some space from the previous element
+    imgWrapper.style.cursor = 'pointer';
+    imgWrapper.onclick = () => {
+        window.open(url, '_blank');
+    }
+    document.querySelectorAll('.logo')[0].appendChild(imgWrapper);
+    return imgWrapper;
+}
+
+// Function to process messages
+const processMessages = (FCADE) => {
+    const globalUsers = FCADE.globalUsers;
+    // Select all message elements and exclude already processed ones
+    const messageElements = document.querySelectorAll('#app div.message:not([data-has-flag])') || [];
+    messageElements.forEach(messageElement => {
+        const authorElement = messageElement.querySelector('span.author');
+        if (!authorElement) return;
+
+        const userKey = authorElement.innerText.trim();
+        const globalUser = globalUsers[userKey];
+        // Check if user data exists
+        if (globalUser && globalUsers[userKey].country) {
+            const countryData = globalUsers[userKey].country;
+            const activeChannelId = FCADE.activeChannelId;
+            const usersList = FCADE.$refs[activeChannelId][0]?.$refs?.usersList || [];
+
+            const userFound = (usersList.$children || []).find(child => child?.user?.id === userKey);
+            
+            const pingImg = userFound?.pingSrc;
+            const rankImg = userFound?.rankSrc;
+            const ping = globalUser?.ping;
+            // create span element for ping
+            const pingTextElement = document.createElement('span');
+            // set small font size for ping
+            pingTextElement.style.fontSize = 'small';
+            pingTextElement.style.marginLeft = '5px';
+            pingTextElement.style.fontWeight = 'normal';
+            pingTextElement.innerHTML = `(ping: ~${ping} ms)`;
+            const statusElement = createStatusElement(globalUser?.away);
+            // Create and append the flag and ping elements
+            const flagElement = createFlagElement(countryData);
+            // add status element as first element
+            authorElement.parentElement.insertBefore(statusElement, authorElement);
+            authorElement.appendChild(flagElement);
+            if(rankImg){
+                const rankElement = createRankElement(rankImg, userFound?.rankTitle);
+                authorElement.appendChild(rankElement);
+            }
+            if(pingImg){
+                const pingElement = createPingElement(pingImg, userFound?.pingTitle);
+                authorElement.appendChild(pingElement);
+            }
+            authorElement.appendChild(pingTextElement);
+            
+            
+
+            // Mark the message as processed
+            messageElement.dataset.hasFlag = "true";
+        }
+    });
+};
+
+
+
 /**
  * Waits for the Vue app to initialize and executes a callback once ready.
  * @param {Function} callback - The callback to execute with the Vue app instance.
@@ -131,6 +283,8 @@ const fightcadePlugins = (fcWindow) => {
             FCADE._data.global.setTheme(theme);
             console.log(`Theme set to: ${theme}`);
         };
+
+        
 
         const showFavorites = () => {
             const searchChannel = FCADE.$children.find((child) => child.$options.name === 'search-channel');
@@ -177,6 +331,14 @@ const fightcadePlugins = (fcWindow) => {
             if (targetElement) {
                 addNavigationButtons(targetElement, () => changeChannel(-1), () => changeChannel(1));
             }
+        }
+        setInterval(()=>{
+            processMessages(FCADE);
+        }, 1000);
+        
+
+        if (CONFIG.addMorePlayerInfoToChat) {
+            console.log({ addMorePlayerInfoToChat: CONFIG.addMorePlayerInfoToChat });
         }
 
         if (CONFIG.showFavoritesButton) {
@@ -225,6 +387,12 @@ const fightcadePlugins = (fcWindow) => {
 
             fcWindow.document.body.appendChild(container);
         }
+
+
+
+        initializeAddons('https://www.honmaru.pl/images/logo/arkadyzja.png', 'https://arkadyzja.honmaru.pl', 'Arkadyzja - DuckStation Rollback Multiplayer Lobby!')
+
+
 
         setTheme(CONFIG.startupTheme);
     });
